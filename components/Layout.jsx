84f5e9376f6ad863c34746d3af7de1f97f41f8b1
rolic-hub <enyiomaeze09@gmail.com>
2022-09-10 20:10:44 +0100

    update frontend

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,9 +3,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import Navbar from "./Navbar";
 
-const Layout = ({ children}) => {
-
-
+const Layout = ({ children }) => {
   const router = useRouter();
   const notClicked =
     "bg-green-600 hover:bg-green-800 rounded-lg p-2 mb-4 font-bold";
@@ -22,26 +20,17 @@ const Layout = ({ children}) => {
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div className="flex">
         <div className="flex w-80 h-full border-r-4">
           <div className="flex flex-col p-10 mt-10 ml-5 h-screen">
-            <button
-              className={notClicked}
-              onClick={_profilepage}
-            >
+            <button className={notClicked} onClick={_profilepage}>
               My Profile
             </button>
-            <button
-              onClick={_fundingPage}
-              className={ notClicked}
-            >
+            <button onClick={_fundingPage} className={notClicked}>
               My Funding Projects
             </button>
-            <button
-              onClick={_fundingProgram}
-              className={ notClicked}
-            >
+            <button onClick={_fundingProgram} className={notClicked}>
               Funding Programs
             </button>
           </div>
